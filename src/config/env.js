813be8e1e41d9f69d07099978108c8bacce71ebb
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2022-05-22 12:05:11 +0100

    Update env configuration

---
+++ src/config/env.js
@@ -0,0 +1,23 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const LOCAL_MYSQL = "mysql://root:@localhost:3306/free-lunch";
+
+const ENV = {
+  jwtSecret: process.env.JWT_SECRET,
+  mongoUrl: process.env.MONGODB,
+  databaseUrl:
+    process.env.NODE_ENV === "development"
+      ? LOCAL_MYSQL
+      : process.env.DATABASE_URL,
+  backendURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:8080"
+      : `https://freelunch.co`,
+  smtpUsername: process.env.SMTP_USER,
+  smtpPassword: process.env.SMTP_PASSWORD,
+  smtpHost: process.env.SMTP_HOST,
+  smtpPort: process.env.SMTP_PORT,
+};
+
+module.exports = ENV;
