4d9aead22448692feba8db3c1a77a78494377b34
Uzo-Felix <uzochukwuonyekwelu03@gmail.com>
2022-06-18 12:09:09 +0100

    feat: lunch routes

---
+++ src/routes/lunch.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const LunchController = require("../controller/lunch");
+const useCatchErrors = require("../error/catchErrors");
+const { isAuthenticated } = require("../middlewares/auth");
+
+class LunchRoute {
+  router = express.Router();
+  lunchController = new LunchController();
+  path = "/lunch";
+
+  constructor() {
+    this.initializeRoutes();
+  }
+
+  initializeRoutes() {
+    this.router.get(
+      `${this.path}/all`,
+      isAuthenticated, //Make sure User is Authenticated
+      useCatchErrors(
+        this.lunchController.getAllLunch.bind(this.lunchController)
+      )
+    );
+    this.router.post(
+      `${this.path}/send`,
+      isAuthenticated,
+      useCatchErrors(this.lunchController.sendLunch.bind(this.lunchController))
+    );
+    this.router.get(
+      `${this.path}/:id`,
+      isAuthenticated,
+      useCatchErrors(this.lunchController.getLunch.bind(this.lunchController))
+    );
+  }
+}
+
+module.exports = LunchRoute;
